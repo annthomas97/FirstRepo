c327f9ec17bb7c5177d53a71d5d6b937ce9f66f9
Ann <Ann@Ann-PC>
2019-09-24 13:18:16 +0530

    first commit

---
+++ DemoProjectGit/src/com/ust/git/Main.java
@@ -0,0 +1,16 @@
+package com.ust.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData objSD = new SalesData();
+		System.out.println("hello guys");
+		displayGreetings();
+		objSD.display();
+	}
+	private static void displayGreetings(){
+		System.out.println("welcome to sales data");
+	}
+	}
+
+
---
+++ DemoProjectGit/src/com/ust/git/SalesData.java
@@ -0,0 +1,8 @@
+package com.ust.git;
+
+public class SalesData {
+	void display(){
+		System.out.println("hello");
+	}
+
+}
